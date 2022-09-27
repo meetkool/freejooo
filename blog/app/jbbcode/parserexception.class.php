7c01d94f0f2f7b1a4dae10d199e6adc91c6c283d
meetkool <lcs2021023@iiitl.ac.in>
2022-09-27 22:28:35 +0530

    add

--- blog/app/jbbcode/parserexception.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace JBBCode;
-
-defined('PROJECT_PATH') OR exit('No direct script access allowed');
-
-use Exception;
-
-class ParserException extends Exception{
-}
--- kool/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Location: http://maxbvtj355hca3scgwtw767mbaqlyhczssddazlyulb43ij63is4gayd.onion');
-
-exit;
-
-?>
